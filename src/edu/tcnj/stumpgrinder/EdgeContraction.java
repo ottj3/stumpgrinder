49433424b46b038340fd8ab5dac42ba00a7d02db
Andrew Miller <andrewmiller237@gmail.com>
2016-04-13 22:24:43 -0400

    Skeleton for EdgeContraction class

---
+++ src/edu/tcnj/stumpgrinder/EdgeContraction.java
@@ -0,0 +1,17 @@
+package edu.tcnj.stumpgrinder;
+
+/**************************************************************************
+ * The edge contraction algorithm.
+ *
+ * @author  Andrew Miller <millea18@tcnj.edu>
+ * @version 0.0
+ **************************************************************************/
+public class
+EdgeContraction
+{
+    public static <T> void
+    edgeContraction(Tree<List<SetList<T>>> tree)
+    {
+
+    }
+}
