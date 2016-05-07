71a77b5a3f5e6d7a630fe0129dff421b923978bd
Andrew Miller <andrewmiller237@gmail.com>
2016-05-06 21:15:05 -0400

    Refactored Pair.java

--- src/edu/tcnj/stumpgrinder/Pair.java
+++ src/edu/tcnj/stumpgrinder/Pair.java
@@ -5,48 +5,56 @@ package edu.tcnj.stumpgrinder;
  * An immutable data type used to group data of different types.
  *
  * @author  Andrew Miller <millea18@tcnj.edu>
- * @version 1.0
+ * @version 0.1
+ * @since   0.1
  **************************************************************************/
-public class
-Pair <T1, T2>
+public class Pair <T1, T2>
 {
-    /** The first element of the pair **/
-    private final
-    T1 first;
+    /** Variables **/
 
-    /** The second element of the pair **/
-    private final
-    T2 second;
+    /**
+     * The first element of the pair
+     */
+    private final T1 first;
+
+    /**
+     * The second element of the pair
+     */
+    private final T2 second;
+
+
+    /** Constructors **/
+
 
     /**************************************************************************
      * Constructs a pair.
+     *
      * @param first  The first element of the pair.
      * @param second The second element of the pair.
      **************************************************************************/
-    public
-    Pair(T1 first, T2 second)
-    {
+    public Pair(T1 first, T2 second)
+      {
         this.first = first;
         this.second = second;
-    }
+      }
 
     /**************************************************************************
      * Returns the first element of this pair.
+     *
      * @return The first element of this pair.
      **************************************************************************/
-    public T1
-    fst()
-    {
+    public T1 fst()
+      {
         return first;
-    }
+      }
 
     /**************************************************************************
      * Returns the second element of this pair.
+     *
      * @return The second element of this pair.
      **************************************************************************/
-    public T2
-    snd()
-    {
+    public T2 snd()
+      {
         return second;
-    }
+      }
 }
