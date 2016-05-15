68a6f1dbd3c667bc5b4f02d36d443dcd2c57c762
Angela Huang <huanga9@tcnj.edu>
2016-05-15 15:31:27 -0400

    Added helper methods toData() and deepClone()

--- src/edu/tcnj/stumpgrinder/Tree.java
+++ src/edu/tcnj/stumpgrinder/Tree.java
@@ -1,10 +1,15 @@
 package edu.tcnj.stumpgrinder;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
-public class Tree<T> implements Serializable{
+public class Tree<T> implements Serializable {
 	/** A list of the nodes in the tree **/
 	private ArrayList<Node<T>> nodes;
 
@@ -18,7 +23,6 @@ public class Tree<T> implements Serializable{
 		this.root = null;
 		this.nodes = new ArrayList<Node<T>>();
 	}
-
 
 	/**************************************************************************
 	 * Constructs a tree with nodes and no root.
@@ -51,6 +55,27 @@ public class Tree<T> implements Serializable{
 	 **************************************************************************/
 	public ArrayList<Node<T>> getNodes() {
 		return this.nodes;
+	}
+
+	/**************************************************************************
+	 * Returns a list of the nodes in this tree.
+	 * 
+	 * @return A list of the nodes in this tree.
+	 **************************************************************************/
+	// ArrayList<Node<T>> allNodes = new ArrayList<Node<T>>();
+	public ArrayList<Node<T>> getAllNodes(ArrayList<Node<T>> nodes) {
+
+		ArrayList<Node<T>> allNodes = nodes;
+		// allNodes.addAll(nodes);
+
+		for (int i = 0; i < allNodes.size(); i++) {
+			if (allNodes.get(i).getChildren().size() > 0) {
+
+				allNodes.addAll(getAllNodes(allNodes.get(i).getChildren()));
+			}
+		}
+
+		return allNodes;
 	}
 
 	/**************************************************************************
@@ -140,7 +165,7 @@ public class Tree<T> implements Serializable{
 	 * @return A string representation of the tree.
 	 **************************************************************************/
 	public String toString() {
-		String string = this.root.getLabel() + ":" + root.getData() + ";";
+		String string = this.root.getLabel() + ":" + root.getCost() + ";";
 		string = toStringRecursive(root) + string;
 		string = string.replace(",)", ")");
 		return string;
@@ -159,7 +184,7 @@ public class Tree<T> implements Serializable{
 		}
 
 		for (Node<T> child : current.getChildren()) {
-			string = child.getLabel() + ":" + child.getData() + "," + string;
+			string = child.getLabel() + ":" + child.getCost() + "," + string;
 			string = toStringRecursive(child) + string;
 		}
 
@@ -169,47 +194,90 @@ public class Tree<T> implements Serializable{
 
 		return string;
 	}
-	
+
+	/**************************************************************************
+	 * Returns a string representation of the tree. (Newick Tree Format)
+	 * Includes the data contained in each node.
+	 * 
+	 * @return A string representation of the tree.
+	 **************************************************************************/
+	public String toStringData() {
+		String string = this.root.getLabel() + ":" + root.getData() + ";";
+		string = toStringDataRecursive(root) + string;
+		string = string.replace(",)", ")");
+		return string;
+	}
+
+	/**************************************************************************
+	 * Returns a string representation of the subtree from a specified node.
+	 * Includes the data contained in each node.
+	 * 
+	 * @return A string representation of the subtree from a specified node.
+	 **************************************************************************/
+	public String toStringDataRecursive(Node<T> current) {
+		String string = "";
+
+		if (current.getChildren().size() > 0) {
+			string = ")" + string;
+		}
+
+		for (Node<T> child : current.getChildren()) {
+			string = child.getLabel() + ":" + child.getData() + "," + string;
+			string = toStringDataRecursive(child) + string;
+		}
+
+		if (current.getChildren().size() > 0) {
+			string = "(" + string;
+		}
+
+		return string;
+	}
+
 	/**************************************************************************
 	 * Returns the node in the tree with the specified label.
-	 * @param label of node
+	 * 
+	 * @param label
+	 *            of node
 	 * @return Node belonging to tree if node is found
 	 **************************************************************************/
-    public Node<T> getNode(String label, Node<T> parent, ArrayList<Node<T>> children)
-    {
-        Node <T> current = new Node<T>();
-        for (Node<T> node : treeNodes) {
-            if (node.getLabel().equals(label) && node.getChildren().equals(children) && node.getParent() == parent) {
-                current = node;
-            }
-            else if (node.getChildren().size()>0){
-            	current = getNode(label, parent, children, node.getChildren());
-            }
-        }
+	public Node<T> getNode(Node<T> edgeNode, Node<T> treeNode) {
+		Node<T> current = new Node<T>();
+		if (treeNode.getLabel().equals(edgeNode.getLabel())
+				&& treeNode.getData().equals(edgeNode.getData())) {
+			return treeNode;
+		} else {
+			if (treeNode.getChildren().size() > 0) {
+				for (Node<T> child : treeNode.getChildren()) {
+					return getNode(edgeNode, child);
+				}
+			}
+		}
 
-        if (this.getRoot().getLabel() == label && this.getRoot().getChildren() == children && this.getRoot().getParent() == parent ) {
-            current = this.getRoot();
-        }
-
-        return current;
-    }
+		return current;
+	}
 
 	/**************************************************************************
-	 * Returns the node in the tree with the specified label or 
-	 * constructs an unlabelled node if the requested node does not exist. 
-	 * @param label of node
-	 * @return Node belonging to tree if node is found; unlabelled node otherwise
+	 * Returns the node in the tree with the specified label or constructs an
+	 * unlabelled node if the requested node does not exist.
+	 * 
+	 * @param label
+	 *            of node
+	 * @return Node belonging to tree if node is found; unlabelled node
+	 *         otherwise
 	 **************************************************************************/
 	public Node<T> createNode(String label) {
 		Node<T> current = new Node<T>("");
 		if (this.getRoot().getLabel().equals(label)) {
-			//current = this.getRoot();
-			current = new Node<T> (this.getRoot().getLabel(), this.getRoot().getData());
+			// current = new Node<T> (this.getRoot().getLabel(),
+			// this.getRoot().getData());
+			current = (Node<T>) deepClone(this.getRoot());
 		} else {
 			for (int i = 0; i < this.nodes.size(); i++) {
 				if (label != "" && this.nodes.get(i).getLabel().equals(label)) {
-					current = new Node<T> (this.nodes.get(i).getLabel(), this.nodes.get(i).getData());
-					//current = this.nodes.get(i);
+					// current = new Node<T> (this.nodes.get(i).getLabel(),
+					// this.nodes.get(i).getData());
+					current = (Node<T>) deepClone(this.nodes.get(i));
+					;
 				}
 			}
 		}
@@ -219,28 +287,40 @@ public class Tree<T> implements Serializable{
 
 	/**************************************************************************
 	 * Returns the root Node of the reconstructed tree.
-	 * @param A string representation of the tree.
+	 * 
+	 * @param A
+	 *            string representation of the tree.
 	 **************************************************************************/
 	public Node<T> fromString(String treeString) {
 		/* Find the root node and make it the parent */
 		int last = treeString.lastIndexOf(':');
-		
+
 		String label = treeString.substring(last - 1, last);
-	
-		return fromStringRecursive(treeString, new Node<T>(this.createNode(label).getLabel(), this.createNode(label).getData()), 0, last);
+
+		return fromStringRecursive(
+				treeString,
+				new Node<T>(this.createNode(label).getLabel(), this.createNode(
+						label).getData()), 0, last);
 	}
 
 	/**************************************************************************
 	 * Recursively builds the tree from its string representation.
-	 * @param A string representation of the tree.
-	 * @param Node 
-	 * @param start index of the string
-	 * @param end index of the string
+	 * 
+	 * @param A
+	 *            string representation of the tree.
+	 * @param Node
+	 * @param start
+	 *            index of the string
+	 * @param end
+	 *            index of the string
 	 * 
 	 **************************************************************************/
-	public Node<T> fromStringRecursive(String s, Node<T> parent, int start, int end) {
-		/*Check if the substring contains a single label.
-		 * If so, return the node. Else, continue parsing the string*/
+	public Node<T> fromStringRecursive(String s, Node<T> parent, int start,
+			int end) {
+		/*
+		 * Check if the substring contains a single label. If so, return the
+		 * node. Else, continue parsing the string
+		 */
 		if (s.charAt(start) != '(') {
 			return parent;
 		}
@@ -248,7 +328,7 @@ public class Tree<T> implements Serializable{
 		int brackets = 0; // counts parenthesis
 		int colon = 0; // marks the position of the colon
 		int marker = start; // marks the position of string
-		String label = ""; //stores the label of the node
+		String label = ""; // stores the label of the node
 
 		for (int i = start; i < end; i++) {
 			char c = s.charAt(i);
@@ -271,10 +351,38 @@ public class Tree<T> implements Serializable{
 					label = "";
 
 				}
-				parent.makeChild(fromStringRecursive(s, new Node<T>(this.createNode(label).getLabel(), this.createNode(label).getData()), marker + 1, colon));
+				parent.makeChild(fromStringRecursive(s, new Node<T>(this
+						.createNode(label).getLabel(), this.createNode(label)
+						.getData()), marker + 1, colon));
 				marker = i;
 			}
 		}
 		return parent;
+	}
+
+	/*******************************************************************************
+	 * Allows for a deep copy of tree object.
+	 * 
+	 * @param object
+	 *            to be copied
+	 * @return a clone of the object
+	 ******************************************************************************/
+	public static Object deepClone(Object object) {
+		try {
+			// Write to object output stream
+			ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+			ObjectOutputStream objectOutput = new ObjectOutputStream(byteOutput);
+			objectOutput.writeObject(object);
+
+			// Read back object through input stream.
+			ByteArrayInputStream byteInput = new ByteArrayInputStream(
+					byteOutput.toByteArray());
+			ObjectInputStream objectInput = new ObjectInputStream(byteInput);
+
+			return objectInput.readObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
