fef955c9b374170e0efddb77c3cca2dfecb1e809
Andrew Miller <andrewmiller237@gmail.com>
2016-05-17 14:17:42 -0400

    Changed top level comment for Node.java

--- src/main/java/edu/tcnj/stumpgrinder/Node.java
+++ src/main/java/edu/tcnj/stumpgrinder/Node.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 /**
- * A node in a tree.
+ * A node in a tree, containing data of type T.
  *
  * @author  Andrew Miller <millea18@tcnj.edu>
  * @version 0.1
