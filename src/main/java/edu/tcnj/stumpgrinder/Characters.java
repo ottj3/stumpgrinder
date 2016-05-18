8cb37b8b9e80f42b344ca3d7e9240151ca324100
Andrew Miller <andrewmiller237@gmail.com>
2016-05-18 09:41:22 -0400

    Reformatted

--- src/main/java/edu/tcnj/stumpgrinder/Characters.java
+++ src/main/java/edu/tcnj/stumpgrinder/Characters.java
@@ -8,203 +8,180 @@ import java.util.List;
 import java.util.Set;
 
 /**************************************************************************
+ *
  * @author  Andrew Miller <millea18@tcnj.edu>
  * @version 0.1
  **************************************************************************/
-public class
-Characters<E>
+public class Characters<E>
 {
-    /** The root set of character states **/
-    private List<Set<E>>
-    rootSet;
+  /**
+   * The root set of character states 
+   */
+  private List<Set<E>> rootSet;
 
-    /** The upper set of character states **/
-    private List<Set<E>>
-    upperSet;
+  /**
+   * The upper set of character states 
+   */
+  private List<Set<E>> upperSet;
 
-    /** The lower set of character states **/
-    private List<Set<E>>
-    lowerSet;
+  /**
+   * The lower set of character states 
+   */
+  private List<Set<E>> lowerSet;
 
-    /** The length of all three sets of character states **/
-    private int
-    length;
+  /** The length of all three sets of character states **/
+  private int length;
 
-    public
-    Characters()
+  public Characters()
     {
-        this.rootSet  = new ArrayList<Set<E>>();
-        this.upperSet = new ArrayList<Set<E>>();
-        this.lowerSet = new ArrayList<Set<E>>();
-        this.length   = 10;
+      this.rootSet  = new ArrayList<Set<E>>();
+      this.upperSet = new ArrayList<Set<E>>();
+      this.lowerSet = new ArrayList<Set<E>>();
+      this.length   = 10;
     }
 
-    public
-    Characters(int initialCapacity)
+  public Characters(int initialCapacity)
     {
-        this.rootSet  = new ArrayList<Set<E>>(initialCapacity);
-        this.upperSet = new ArrayList<Set<E>>(initialCapacity);
-        this.lowerSet = new ArrayList<Set<E>>(initialCapacity);
-        this.length   = initialCapacity;
+      this.rootSet  = new ArrayList<Set<E>>(initialCapacity);
+      this.upperSet = new ArrayList<Set<E>>(initialCapacity);
+      this.lowerSet = new ArrayList<Set<E>>(initialCapacity);
+      this.length   = initialCapacity;
     }
 
-    /**************************************************************************
-     **************************************************************************/
-    public List<Set<E>>
-    getRootSet()
+  /**************************************************************************
+   **************************************************************************/
+  public List<Set<E>> getRootSet()
     {
-        return rootSet;
+      return rootSet;
+  }
+
+  public List<Set<E>> getUpperSet()
+    {
+      return upperSet;
     }
 
-    public List<Set<E>>
-    getUpperSet()
+  public List<Set<E>> getLowerSet()
     {
-        return upperSet;
+      return lowerSet;
     }
 
-    public List<Set<E>>
-    getLowerSet()
+  public void setRootSet(List<Set<E>> rootSet)
     {
-        return lowerSet;
+      this.rootSet = rootSet;
     }
 
-    public void
-    setRootSet(List<Set<E>> rootSet)
+  public void setUpperSet(List<Set<E>> upperSet)
     {
-        this.rootSet = rootSet;
+      this.upperSet = upperSet;
     }
 
-    public void
-    setUpperSet(List<Set<E>> upperSet)
+  public void setLowerSet(List<Set<E>> lowerSet)
     {
-        this.upperSet = upperSet;
+      this.lowerSet = lowerSet;
     }
 
-    public void
-    setLowerSet(List<Set<E>> lowerSet)
+  public Set<E> getFromRootSet(int index)
     {
-        this.lowerSet = lowerSet;
+      return rootSet.get(index);
     }
 
-    public Set<E>
-    getFromRootSet(int index)
+  public Set<E> getFromUpperSet(int index)
     {
-        return rootSet.get(index);
+      return upperSet.get(index);
     }
 
-    public Set<E>
-    getFromUpperSet(int index)
+  public Set<E> getFromLowerSet(int index)
     {
-        return upperSet.get(index);
+      return upperSet.get(index);
     }
 
-    public Set<E>
-    getFromLowerSet(int index)
+  public void setToRootSet(int index, E element)
     {
-        return upperSet.get(index);
+      Set<E> set = new HashSet<E>();
+      set.add(element);
+
+      rootSet.set(index, set);
     }
 
-    public void
-    setToRootSet(int index, E element)
+  public void setToRootSet(int index, Set<E> set)
     {
-        Set<E> set = new HashSet<E>();
-        set.add(element);
-
-        rootSet.set(index, set);
+      rootSet.set(index, set);
     }
 
-    public void
-    setToRootSet(int index, Set<E> set)
+  public void setToUpperSet(int index, E element)
     {
-        rootSet.set(index, set);
+      Set<E> set = new HashSet<E>();
+      set.add(element);
+
+      upperSet.set(index, set);
     }
 
-    public void
-    setToUpperSet(int index, E element)
+  public void setToUpperSet(int index, Set<E> set)
     {
-        Set<E> set = new HashSet<E>();
-        set.add(element);
-
-        upperSet.set(index, set);
+      upperSet.set(index, set);
     }
 
-    public void
-    setToUpperSet(int index, Set<E> set)
+  public void setToLowerSet(int index, E element)
     {
-        upperSet.set(index, set);
+      Set<E> set = new HashSet<E>();
+      set.add(element);
+
+      lowerSet.set(index, set);
     }
 
-    public void
-    setToLowerSet(int index, E element)
+  public void setToLowerSet(int index, Set<E> set)
     {
-        Set<E> set = new HashSet<E>();
-        set.add(element);
-
-        lowerSet.set(index, set);
+      lowerSet.set(index, set);
+    }
+  
+  public void addToRootSet(int index, E element)
+    {
+      Set<E> set = new HashSet<E>();
+      set.add(element);
+      
+      this.rootSet.add(set);
     }
 
-    public void
-    setToLowerSet(int index, Set<E> set)
+  public void addToRootSet(int index, Set<E> set)
     {
-        lowerSet.set(index, set);
-    }
-    
-    public void
-    addToRootSet(int index, E element)
-    {
-        Set<E> set = new HashSet<E>();
-        set.add(element);
-        
-        this.rootSet.add(set);
+      this.upperSet.add(set);
     }
 
-    public void
-    addToRootSet(int index, Set<E> set)
+  public void addToUpperSet(int index, E element)
     {
-        this.upperSet.add(set);
+      Set<E> set = new HashSet<E>();
+      set.add(element);
+      
+      this.upperSet.add(set);
     }
 
-    public void
-    addToUpperSet(int index, E element)
+  public void addToUpperSet(int index, Set<E> set)
     {
-        Set<E> set = new HashSet<E>();
-        set.add(element);
-        
-        this.upperSet.add(set);
+      this.upperSet.add(set);
     }
 
-    public void
-    addToUpperSet(int index, Set<E> set)
+  public void addToLowerSet(int index, E element)
     {
-        this.upperSet.add(set);
+      Set<E> set = new HashSet<E>();
+      set.add(element);
+      
+      this.lowerSet.add(set);
     }
 
-    public void
-    addToLowerSet(int index, E element)
+  public void addToLowerSet(int index, Set<E> set)
     {
-        Set<E> set = new HashSet<E>();
-        set.add(element);
-        
-        this.lowerSet.add(set);
+      this.lowerSet.add(set);
     }
 
-    public void
-    addToLowerSet(int index, Set<E> set)
+  public int size()
     {
-        this.lowerSet.add(set);
+      return this.length;
     }
 
-    public int
-    size()
+  public String toString()
     {
-        return this.length;
-    }
-
-    public String
-    toString()
-    {
-        return "Root:  " + this.rootSet.toString() + 
-               "Upper: " + this.upperSet.toString() +
-               "Lower: " + this.lowerSet.toString();
+      return "Root:  " + this.rootSet.toString() + 
+             "Upper: " + this.upperSet.toString() +
+             "Lower: " + this.lowerSet.toString();
     }
 }
